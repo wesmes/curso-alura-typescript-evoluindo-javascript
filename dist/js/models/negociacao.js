a1fe8477779c9d10efd952e4091b14f5226da11b
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-23 06:09:18 -0300

    Módulo 1 - Aula 7: Negociação, modelagem e regras

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,13 @@
+export class Negociacao {
+
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+}
