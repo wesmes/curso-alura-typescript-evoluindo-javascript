0dba0ebb4f69f00e11a124e8d337f7310215a4ba
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-25 07:00:05 -0300

    Módulo 3 - Aula 11: Organizando melhor nosso código

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -7,11 +7,14 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
         const exp = /-/g;
         const data = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
-        const negociacao = new Negociacao(data, quantidade, valor);
-        console.log(negociacao);
+        return new Negociacao(data, quantidade, valor);
     }
 }
