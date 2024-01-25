93b22637301ff5927d9d09ebc2ed809645a5f06b
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-25 06:33:49 -0300

    Módulo 3 - Aula 4: Surpresa ao iniciar uma negociação

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,3 +1,4 @@
+import { Negociacao } from "../models/negociacao.js";
 export class NegociacaoController {
     constructor() {
         // Fazendo o casting para HTMLInputElement
@@ -6,8 +7,7 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValor);
+        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        console.log(negociacao);
     }
 }
