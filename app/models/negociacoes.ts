0ee64b3c7e5e40ea730db8a79d13c29712c71a30
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-02-19 08:14:45 -0300

    Módulo 4 - Aula 5: Avançando com nosso modelo

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,4 +2,12 @@ import { Negociacao } from "./negociacao";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao): void {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao> {
+        return this.negociacoes;
+    }
 }
