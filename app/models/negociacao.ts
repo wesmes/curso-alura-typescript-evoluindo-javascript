96dc2bb0202e6ff7b0829ffdd16e1ac8d5b643b2
Weslley Gomes <wegomes@santillana.com>
2024-07-11 17:54:15 -0300

    Módulo 5 - Aula 8: Programação defensiva

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,13 +1,20 @@
 export class Negociacao {
 
     constructor(
-        public readonly data: Date, 
+        private _data: Date,
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
 
     get volume(): number {
         return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        // Alterna a referência da variável na memória
+        // Para evitar que o método Date.setDate() altere a data da negociação
+        const data = new Date(this._data.getTime());
+        return data;
     }
 
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,10 +1,16 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
     get volume() {
         return this.quantidade * this.valor;
+    }
+    get data() {
+        // Alterna a referência da variável na memória
+        // Para evitar que o método Date.setDate() altere a data da negociação
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
